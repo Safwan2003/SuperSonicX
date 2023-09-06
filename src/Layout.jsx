219df1faf25e29f25a84ee084da0102ec86ccbb0
SAFWAN ALI <126313311+Safwan2003@users.noreply.github.com>
2023-09-06 15:11:56 +0500

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -12,14 +12,10 @@ const Layout = () => {
 
   return (
     <div className="">
-
-      <div className=' flex justify-between lg:justify-center  p-4 py-20 body '>
-     
-    
-        <h3 data-aos="fade-down" className=' text-4xl px-10 lg:absolute left-5' id='#home'>              <Link  to="/">
-Supersonic Themes </Link></h3> 
-       
-       
+      <div className='flex justify-between lg:justify-center p-4 py-20 body '>
+        <h3 data-aos="fade-down" className='text-4xl px-10 lg:absolute left-5' id='#home'>
+          <Link to="/">Supersonic Themes</Link>
+        </h3>
         <button
           className="lg:hidden text-3xl focus:outline-none absolute top-15 right-5"
           onClick={toggleMenu}
@@ -29,7 +25,7 @@ Supersonic Themes </Link></h3>
         <nav className={`mt-4 lg:mt-0 lg:ml-8 ${menuOpen ? "" : "hidden"} lg:flex lg:space-x-8 `}>
           <ul className="lg:flex lg:space-x-10 text-3xl lg:justify-center">
             <li>
-              <Link  to="/">Home <span></span></Link>
+              <Link to="/">Home <span></span></Link>
             </li>
             <li>
               <Link to="/about">About<span></span></Link>
@@ -41,28 +37,21 @@ Supersonic Themes </Link></h3>
               <Link to="/Contact">Contact <span></span></Link>
             </li>
           </ul>
-        
-        
         </nav>
         <div className='center lg:absolute right-8 top-12'>
-        <div className='outer Button'>
-        <div className="mt-4 lg:mt-0">
-          <button className="header-btn text-2xl bg-blue-200 p-1 rounded-lg px-3 "><Link to="/Contact">LET'S TALK</Link></button>
-        </div>
-        </div>
+          <div className='outer Button'>
+            <div className="mt-4 lg:mt-0">
+              <button className="header-btn text-2xl bg-blue-200 p-1 rounded-lg px-3 ">
+                <Link to="/Contact">LET'S TALK</Link>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-
       <Outlet />
-     <Footer/>
+      <Footer />
     </div>
-
-
-
-        
-
-
-  )
+  );
 };
 
 export default Layout;
