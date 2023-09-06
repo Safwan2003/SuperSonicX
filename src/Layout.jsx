21401d04aaa3989d52459ea44c737684ff301169
SAFWAN ALI <126313311+Safwan2003@users.noreply.github.com>
2023-09-06 15:09:59 +0500

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -43,14 +43,14 @@ Supersonic Themes </Link></h3>
           </ul>
         
         
-        </div>
+        </nav>
         <div className='center lg:absolute right-8 top-12'>
         <div className='outer Button'>
         <div className="mt-4 lg:mt-0">
           <button className="header-btn text-2xl bg-blue-200 p-1 rounded-lg px-3 "><Link to="/Contact">LET'S TALK</Link></button>
         </div>
         </div>
-        </nav>
+        </div>
       </div>
 
       <Outlet />
